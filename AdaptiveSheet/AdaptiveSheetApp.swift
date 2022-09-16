3eafa569c68a3f074ba12e9f123fa23e4b1f2bd3
Trevor <85640211+trevordevelops@users.noreply.github.com>
2022-09-16 09:54:43 -0700

    Initial Commit

---
+++ AdaptiveSheet/AdaptiveSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveSheetApp.swift
+//  AdaptiveSheet
+//
+//  Created by Trevor Welsh on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
